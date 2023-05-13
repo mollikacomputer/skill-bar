8d10033a7da40a2ebf6396bcd3dbdab846bfa432
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-13 10:19:03 +0600

    some added

--- components/Skillbar.js
+++ components/Skillbar.js
@@ -9,48 +9,54 @@ const SkillBar = () => {
           <div className={styles.skillBox}>
             <span className={styles.title}>HTML </span>
             <div className={styles.skillBar}>
-              <span className={`${styles.skillPer} ${styles.html}`}></span>
-              <span className="tooltip">95%</span>
+              <span className={`${styles.skillPer} ${styles.html}`}>
+              <span className={styles.tooltip}>95%</span>
+              </span>
             </div>
           </div>
           {/* css skill part */}
           <div className={styles.skillBox}>
             <span className={styles.title}>CSS</span>
             <div className={styles.skillBar}>
-              <span className={`${styles.skillPer} ${styles.css}`}></span>
-              <span className="tooltip">90%</span>
+              <span className={`${styles.skillPer} ${styles.css}`}>
+              <span className={styles.tooltip}>90%</span>
+              </span>
             </div>
           </div>
           {/* javascript skill part */}
           <div className={styles.skillBox}>
             <span className={styles.title}>JavaScript </span>
             <div className={styles.skillBar}>
-              <span className={`${styles.skillPer} ${styles.javascript}`}></span>
-              <span className="tooltip">70%</span>
+              <span className={`${styles.skillPer} ${styles.javascript}`}>
+              <span className={styles.tooltip}>70%</span>
+              </span>
             </div>
           </div>
           {/* MongoDb server skill part */}
           <div className={styles.skillBox}>
             <span className={styles.title}> MongoDB </span>
             <div className={styles.skillBar}>
-              <span className={`${styles.skillPer} ${styles.mongodb}`}></span>
-              <span className="tooltip">70%</span>
+              <span className={`${styles.skillPer} ${styles.mongodb}`}>
+              <span className={styles.tooltip}>70%</span>
+              </span>
             </div>
           </div>
           {/* MongoDb server skill part */}
           <div className={styles.skillBox}>
             <span className={styles.title}> NodeJs </span>
             <div className={styles.skillBar}>
-              <span className={`${styles.skillPer} ${styles.node}`}></span>
-              <span className="tooltip">70%</span>
+              <span className={`${styles.skillPer} ${styles.node}`}>
+              <span className={styles.tooltip}>70%</span>
+              </span>
             </div>
           </div>
           {/* Firebase server skill part */}
           <div className={styles.skillBox}>
             <span className={styles.title}> Google Firebase </span>
             <div className={styles.skillBar}>
-              <span className={`${styles.skillPer} ${styles.firebase}`}></span>
-              <span className="tooltip">90%</span>
+              <span className={`${styles.skillPer} ${styles.firebase}`}>
+              <span className={styles.tooltip}>90%</span>
+              </span>
             </div>
           </div>
 
